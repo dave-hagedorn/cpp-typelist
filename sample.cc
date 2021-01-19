af6b913c76819ea12b722ddfc9558fe820658680
Dave Hagedorn <48412044+dave-hagedorn@users.noreply.github.com>
2021-01-18 20:46:52 -0500

    Update sample.cc

--- sample.cc
+++ sample.cc
@@ -1,9 +1,40 @@
-#include <memory>
+/**
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2021, Dave Hagedorn
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <cxxabi.h>
-#include <variant>
 #include <iostream>
+#include <memory>
+#include <variant>
 #include <sstream>
-#include <cxxabi.h>
 
 #include "typelist.hh"
 
@@ -47,7 +78,7 @@ void print(const T& ...things) {
 namespace types = dhagedorn::types;
 
 int main() {
-    using list = types::type_list<double, float, int, char, int, char, float, double>;
+    using list = types::typelist<double, float, int, char, int, char, float, double>;
     constexpr auto size = list::size;
     constexpr auto has_double = list::any_of<[]<typename T>(){ return std::is_same_v<T,double>; }>;
     constexpr auto is_mathy = list::any_of<[]<typename T>(){ return std::is_integral_v<T> || std::is_floating_point_v<T>; }>;
@@ -57,10 +88,12 @@ int main() {
     using with_void = list::push_front<void>;
     using set = list::set;
     using no_floats = list::filter<[]<typename T>(){ return !std::is_floating_point_v<T>; }>;
+    using odds = list::filter<[]<typename T, auto I, types::is_typelist list>(){ return I%2 == 1; }>;
     using sliced = list::slice<0,3>;
     using first_integral = list::find_if<[]<typename T>(){ return std::is_integral_v<T>; }>;
     using first_type = list::at<0>;
     constexpr auto sizes = list::transform_v<[]<typename T>(){ return sizeof(T); }>;
+    constexpr auto indices = list::transform_v<[]<typename T, auto I>(){ return I; }>;
     using pointy = list::transform_with<[]<typename T>() -> T* { return nullptr; }>;
     using safe_pointy = list::transform<std::shared_ptr>;
     using sorted = list::sort<>;
@@ -79,10 +112,12 @@ int main() {
     print("with_void:           ", typeid(with_void));
     print("set:                 ", typeid(set));
     print("no_floats:           ", typeid(no_floats));
+    print("odds:                ", typeid(odds));
     print("sliced:              ", typeid(sliced));
     print("first_integral:      ", typeid(first_integral));
     print("first_type:          ", typeid(first_type));
     print("sizes:               ", sizes);
+    print("indices:             ", indices);
     print("pointy:              ", typeid(pointy));
     print("safe_pointy:         ", typeid(safe_pointy));
     print("sorted:              ", typeid(sorted));

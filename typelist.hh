d92cb6988feb1273f2b5248950f1fc80c4b05a7e
Dave Hagedorn <48412044+dave-hagedorn@users.noreply.github.com>
2021-01-18 20:40:02 -0500

    Update typelist.hh

--- typelist.hh
+++ typelist.hh
@@ -1,3 +1,35 @@
+/**
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2021, Dave Hagedorn
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #pragma once
 
 #include <type_traits>
@@ -21,11 +53,11 @@ template<auto PREDICATE>
 concept is_iter_predicate = is_simple_iter_predicate<PREDICATE> || is_complex_iter_predicate<PREDICATE>;
 
 template<typename ...TYPES>
-struct type_list;
+struct typelist;
 
 template<auto PREDICATE>
 concept is_complex_filter_predicate = requires {
-    { PREDICATE.template operator()<int, std::size_t{0}, type_list<>>() } -> std::convertible_to<bool>;
+    { PREDICATE.template operator()<int, std::size_t{0}, typelist<>>() } -> std::convertible_to<bool>;
 };
 
 template<auto PREDICATE>
@@ -45,7 +77,7 @@ template<typename T>
 struct is_typelist_t: std::false_type {};
 
 template<typename ...TYPES>
-struct is_typelist_t<type_list<TYPES...>>: std::true_type {};
+struct is_typelist_t<typelist<TYPES...>>: std::true_type {};
 
 template<typename T>
 concept is_typelist = is_typelist_t<T>::value;
@@ -53,7 +85,7 @@ concept is_typelist = is_typelist_t<T>::value;
 
 /**
  * Invalid type - used to indicate an empty/non-existent type
- * Ex - a type_list<>::find call that does not find a matching type
+ * Ex - a typelist<>::find call that does not find a matching type
  */
 struct no_type {};
 
@@ -69,7 +101,7 @@ static constexpr auto false_type_v = false;
  * type lists or constexpr values
  */
 template<typename ...TYPES>
-struct type_list {
+struct typelist {
 private:
 template<typename ...OTHER>
     struct holder {
@@ -95,7 +127,7 @@ template<typename ...OTHER>
         } else {
             return PREDICATE.template operator()<T, I, LIST>();
         }
-    }    
+    }
 
     template<auto PREDICATE, auto I, is_typelist LIST>
     static LIST* filter_impl() {
@@ -109,9 +141,9 @@ template<typename ...OTHER>
         if constexpr (PASS) {
             using NEW_LIST = typename LIST::template push_back<FIRST>;
             if constexpr (is_filter_predicate<PREDICATE>) {
-                return filter_impl<PREDICATE, I+1, NEW_LIST, REST...>();   
+                return filter_impl<PREDICATE, I+1, NEW_LIST, REST...>();
             } else {
-                return filter_impl<PREDICATE, I+1, NEW_LIST, REST...>();   
+                return filter_impl<PREDICATE, I+1, NEW_LIST, REST...>();
             }
         } else {
             return filter_impl<PREDICATE, I+1, LIST, REST...>();
@@ -121,7 +153,7 @@ template<typename ...OTHER>
     template<auto PREDICATE, auto I>
     static no_type* find_if_impl() {
         return nullptr;
-    }    
+    }
 
     template<auto PREDICATE, auto I, typename FIRST, typename ...REST>
     static auto* find_if_impl() {
@@ -145,7 +177,7 @@ template<typename ...OTHER>
 
     template<auto PREDICATE, auto ...INDICES>
     static constexpr auto* transform_impl(std::index_sequence<INDICES...>) {
-        return static_cast<type_list<
+        return static_cast<typelist<
             decltype(invoke_iter_predicate<PREDICATE, TYPES, INDICES>())...
         >*>(nullptr);
     }
@@ -160,13 +192,13 @@ template<typename ...OTHER>
         if constexpr (DONE_PASS) {
             if constexpr (LESS_THAN_OR_EQUAL) {
                 if constexpr (NO_SWAP) {
-                    return static_cast<type_list<LIST..., FIRST, SECOND>*>(nullptr);
+                    return static_cast<typelist<LIST..., FIRST, SECOND>*>(nullptr);
                 } else {
                     return sortImpl2<PREDICATE, true, LIST..., FIRST, SECOND>(holder<>{});
                 }
             } else {
                 if constexpr (NO_SWAP) {
-                    return static_cast<type_list<LIST..., SECOND, FIRST>*>(nullptr);
+                    return static_cast<typelist<LIST..., SECOND, FIRST>*>(nullptr);
                 } else {
                     return sortImpl2<PREDICATE, true, LIST..., SECOND, FIRST>(holder<>{});
                 }
@@ -180,7 +212,7 @@ template<typename ...OTHER>
 
     template<template <typename ...> class FROM, typename ...OTHER>
     constexpr static auto* fromImpl(FROM<OTHER...>*) {
-        return static_cast<type_list<OTHER...>*>(nullptr);
+        return static_cast<typelist<OTHER...>*>(nullptr);
     }
 
 public:
@@ -208,7 +240,7 @@ public:
      */
     template<auto PREDICATE>
     requires is_iter_predicate<PREDICATE>
-    constexpr static bool any_of = []<auto ...I>(std::index_sequence<I...>){ 
+    constexpr static bool any_of = []<auto ...I>(std::index_sequence<I...>){
         return (invoke_iter_predicate<PREDICATE, TYPES, I>() || ...);
     }(indices);
 
@@ -217,7 +249,7 @@ public:
      */
     template<auto PREDICATE>
     requires is_iter_predicate<PREDICATE>
-    constexpr static bool all_of = []<auto ...I>(std::index_sequence<I...>){ 
+    constexpr static bool all_of = []<auto ...I>(std::index_sequence<I...>){
         return (invoke_iter_predicate<PREDICATE, TYPES, I>() || ...);
     }(indices);
 
@@ -229,33 +261,33 @@ public:
     constexpr static bool none_of = !any_of<PREDICATE>;
 
     /**
-     * Evaluates to true if this type_list contains all of the types in OTHER
+     * Evaluates to true if this typelist contains all of the types in OTHER
      */
     template<typename ...OTHER>
-    constexpr static auto contains = ((contains_one<OTHER> )&& ...); 
+    constexpr static auto contains = ((contains_one<OTHER> )&& ...);
 
     /**
-     * Append the provided types to this list, returning a new type_list
+     * Append the provided types to this list, returning a new typelist
      */
     template<typename ...OTHER>
-    using push_back = type_list<TYPES..., OTHER...>;
+    using push_back = typelist<TYPES..., OTHER...>;
 
     /**
-     * Prepend the provided types to this list, returning a new type_list
+     * Prepend the provided types to this list, returning a new typelist
      */
     template<typename ...OTHER>
-    using push_front = type_list<OTHER..., TYPES...>;
+    using push_front = typelist<OTHER..., TYPES...>;
 
     /**
-     * Generate a new type_list by filtering the types in this list using the provided
+     * Generate a new typelist by filtering the types in this list using the provided
      * predicate PREDICATE<T>() or PREDICATE<T, INDEX, CURRENT_TYPE_LIST>()
      */
     template<auto PREDICATE>
     requires is_filter_predicate<PREDICATE>
-    using filter = std::remove_reference_t<decltype(*filter_impl<PREDICATE, 0, type_list<>, TYPES...>())>;
+    using filter = std::remove_reference_t<decltype(*filter_impl<PREDICATE, 0, typelist<>, TYPES...>())>;
 
     /**
-     * Generate a new type_list that is a set of this type_list - that is, no duplicate entries
+     * Generate a new typelist that is a set of this typelist - that is, no duplicate entries
      * note:  This form of using-wrapping-another-using seems to be OK (doesn't crash gcc).  The
      *        difference seems to be the top-level using expression doesn't have template arguments in this case
      */
@@ -264,14 +296,14 @@ public:
     }>;
 
     /**
-     * Generate a new type_list by taking the types with indices [FROM,TO)
+     * Generate a new typelist by taking the types with indices [FROM,TO)
      * (typical slice operation)
      * note:  The bizarre unwrapping below is because remove_pointer_t crashes on gcc trunk, and
      *        calling directly as `filter<PREDICATE>;` also crashes gcc trunk
      */
     template<std::size_t FROM, std::size_t TO>
     requires (FROM <= size && TO <= size && FROM < TO)
-    using slice = std::remove_reference_t<decltype(*filter_impl<[]<typename TYPE, auto I, typename LIST>(){ return FROM <= I && I < TO; }, 0, type_list<>, TYPES...>())>;    
+    using slice = std::remove_reference_t<decltype(*filter_impl<[]<typename TYPE, auto I, typename LIST>(){ return FROM <= I && I < TO; }, 0, typelist<>, TYPES...>())>;
 
     /**
      * Return the first type matching the provided PREDICATE
@@ -289,7 +321,7 @@ public:
     using at = std::remove_reference_t<decltype(*find_if_impl<[]<typename T, auto I>(){ return I == INDEX; }, 0, TYPES...>())>;
 
     /**
-     * Transform this type_list into a std::array by converting its
+     * Transform this typelist into a std::array by converting its
      * types into values using PREDICATE
      */
     template<auto PREDICATE>
@@ -297,7 +329,7 @@ public:
     static constexpr auto transform_v = transform_v_impl<PREDICATE>(indices);
 
     /**
-     * Transform this type_list into another type_list using the return type
+     * Transform this typelist into another typelist using the return type
      * of the provided PREDICATE as a guide
      */
     template<auto PREDICATE>
@@ -305,10 +337,10 @@ public:
     using transform_with = std::remove_reference_t<decltype(*transform_impl<PREDICATE>(indices))>;
 
     /**
-     * Transform this type_list into another type_list using the provided type as a guide
+     * Transform this typelist into another typelist using the provided type as a guide
      */
     template<template <typename> class FROM>
-    using transform = type_list<FROM<TYPES>...>;  
+    using transform = typelist<FROM<TYPES>...>;
 
     /**
      * Stable sort - ascending
@@ -321,110 +353,17 @@ public:
 
     /**
      * Inject the types in this list into the provided template class
-     * ex:  type_list::as<std::variant>;
+     * ex:  typelist::as<std::variant>;
      */
     template<template <typename ...> class AS>
     using as = AS<TYPES...>;
 
     /**
      * Extrat the types from the provided template type, injecting
-     * them into a new type_list
+     * them into a new typelist
      */
     template<typename TYPE>
     using from = std::remove_reference_t<decltype(*fromImpl(static_cast<TYPE*>(nullptr)))>;
 };
-
-}
-
-
-#include <memory>
-#include <cxxabi.h>
-#include <variant>
-#include <iostream>
-#include <sstream>
-#include <cxxabi.h>
-
-auto format(const auto& anything) {
-    return anything;
-}
-
-auto format(const std::type_info& tinfo) {
-    auto demangled = abi::__cxa_demangle(tinfo.name(), nullptr, nullptr, nullptr);
-    std::string out = demangled;
-    free(demangled);
-    return out;
-}
-
-template<typename CONT>
-requires (requires (CONT x) { std::cout << format(*x.begin()) << format(*x.end()); })
-auto format(const CONT& cont) {
-    std::stringstream out;
-
-    for (const auto& e: cont) {
-        out << format(e) << ", ";
-    }
-
-    out.seekp(-2, std::ios_base::end);
-    out << '\0';
-    return out.str();
-}
-
-template<typename ...T>
-requires (requires (T ...x) { ((std::cout << format(x)) , ...); })
-void print(const T& ...things) {
-    auto printer = [](const auto& thing) {
-        std::cout << format(thing);
-    };
-
-    ((printer(things)) , ...);
-    std::cout << std::endl;
-
-}
-
-namespace types = dhagedorn::types;
-
-int main() {
-    using list = types::type_list<double, float, int, char, int, char, float, double>;
-    constexpr auto size = list::size;
-    constexpr auto has_double = list::any_of<[]<typename T>(){ return std::is_same_v<T,double>; }>;
-    constexpr auto is_mathy = list::any_of<[]<typename T>(){ return std::is_integral_v<T> || std::is_floating_point_v<T>; }>;
-    constexpr auto is_not_stringy = list::none_of<[]<typename T>(){ return std::is_same_v<T, std::string>; }>;
-    constexpr auto has_int = list::contains<int>;
-    using with_string = list::push_back<std::string>;
-    using with_void = list::push_front<void>;
-    using set = list::set;
-    using no_floats = list::filter<[]<typename T>(){ return !std::is_floating_point_v<T>; }>;
-    using sliced = list::slice<0,3>;
-    using first_integral = list::find_if<[]<typename T>(){ return std::is_integral_v<T>; }>;
-    using first_type = list::at<0>;
-    constexpr auto sizes = list::transform_v<[]<typename T>(){ return sizeof(T); }>;
-    using pointy = list::transform_with<[]<typename T>() -> T* { return nullptr; }>;
-    using safe_pointy = list::transform<std::shared_ptr>;
-    using sorted = list::sort<>;
-    using sorted_backwards = list::sort<[]<typename A, typename B>(){ return sizeof(B) < sizeof(A); }>;
-    using variant = list::as<std::variant>;
-    using from_variant = list::from<variant>;
-
-
-    print("list:                ", typeid(list));
-    print("size:                ", size);
-    print("has_double:          ", has_double);
-    print("is_mathy:            ", is_mathy);
-    print("is_not_stringy:      ", is_not_stringy);
-    print("has_int:             ", has_int);
-    print("with_string:         ", typeid(with_string));
-    print("with_void:           ", typeid(with_void));
-    print("set:                 ", typeid(set));
-    print("no_floats:           ", typeid(no_floats));
-    print("sliced:              ", typeid(sliced));
-    print("first_integral:      ", typeid(first_integral));
-    print("first_type:          ", typeid(first_type));
-    print("sizes:               ", sizes);
-    print("pointy:              ", typeid(pointy));
-    print("safe_pointy:         ", typeid(safe_pointy));
-    print("sorted:              ", typeid(sorted));
-    print("sorted_backwards:    ", typeid(sorted_backwards));
-    print("variant:             ", typeid(variant));
-    print("from_variant:        ", typeid(from_variant));
 
 }
